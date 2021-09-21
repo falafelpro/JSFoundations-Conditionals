ba3ee8a90f209994c0c007323e9f862bc4aedb90
falafelpro <47983933+falafelpro@users.noreply.github.com>
2021-09-21 18:03:58 +0300

    Project's First Commit

--- calculator.js
+++ calculator.js
@@ -2,5 +2,31 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+
+const numOne = +prompt("Please enter the first number: ");
+const numTwo = +prompt("Please enter the second number: ");
+const operation = prompt("Please choose the operation (+, -, /, *):");
+
+const mathOps = ["+","-","/","*"]
+let redFlag = false
+if (isNaN(numTwo) || isNaN(numOne) ){
+    console.log("please enter real numbers");
+    redFlag = true;
+}
+if (!mathOps.includes(operation)){
+    console.log("please only enter these symbols (+, -, /, *)");
+    redFlag = true;
+}
+
+if(!redFlag){
+    if(operation === "+") 
+    console.log(`${numOne} ${operation} ${numTwo} =  ${ numOne + numTwo}`)
+    if(operation === "-") 
+    console.log(`${numOne} ${operation} ${numTwo} =  ${ numOne - numTwo}`)
+    if(operation === "/") 
+    console.log(`${numOne} ${operation} ${numTwo} =  ${ numOne / numTwo}`)
+    if(operation === "*") 
+    console.log(`${numOne} ${operation} ${numTwo} =  ${ numOne * numTwo}`)
+}
+
+
